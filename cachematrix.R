8cd9cf1317746209381015b6e30096deda5f0908
bvianna1 <bvianna1@gmail.com>
2015-06-17 22:46:27 -0300

    Program Assignment 2
    Create a matrix inverse with the usage of cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "matrix", which is really 
+##a matrix containing a function to:
+
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the solve
+##get the value of the solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) m <<- solve
+      getsolve <- function() m
+      list(set = set, get = get,
+            setsolve = setsolve,
+            getsolve = getsolve)
+      
 }
-
 
 ## Write a short comment describing this function
 
